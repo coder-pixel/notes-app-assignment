bd4fc594ae23f06c71f907ea0452982ec208b7d2
sauvik <sauvik@logic-square.com>
2025-05-23 14:40:45 +0530

    feat: PWa support added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Offline Notes App",
+        short_name: "Notes",
+        description: "An offline-first Markdown notes app",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });

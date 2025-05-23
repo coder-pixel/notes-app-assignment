59c29b1cb3bfba14ac8a87ccc3391f2ec3126565
sauvik <sauvik@logic-square.com>
2025-05-23 12:14:17 +0530

    update: changed backend url for mock backend

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,3 @@
-export const BACKEND_BASE_URL = "http://localhost:3001"; // json-server URL
+// export const BACKEND_BASE_URL = "http://localhost:3001"; // json-server URL
+export const BACKEND_BASE_URL =
+  "https://notes-app-assignment-backend.onrender.com"; // json-server URL - mock backend deployed on render
